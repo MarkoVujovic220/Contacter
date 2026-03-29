7d6a363f9c4841f4ae372b20780e42bf3b2e8277
MarkoVujovic220 <122290990+MarkoVujovic220@users.noreply.github.com>
2026-03-29 04:42:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def send_email():
     try:
         sender = "oltenweb@gmail.com"
         receiver = "oltenweb@gmail.com"
-        password = "gwld bkig nvdi bssr" #os.getenv("EMAIL_PASSWORD")
+        password = "gwldbkignvdibssr" #os.getenv("EMAIL_PASSWORD")
 
         message = "Subject: Test Email\n\nThis is a test email from your app 🚀"
 
